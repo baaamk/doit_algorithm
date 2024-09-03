c62855e85f30593c147526fb25cbb1f25d3c5b78
banghyunwoo <brandon55321@gmail.com>
2024-09-03 23:42:34 +0900

    PS

--- src/basic/Gogo.java
+++
@@ -1,7 +0,0 @@
-package basic;
-
-public class Gogo {
-    public static void main(String[] args) {
-        System.out.println(".d = ");
-    }
-}
---
+++ src/basic/P2018_006.java
@@ -0,0 +1,29 @@
+package basic;
+
+import java.util.Scanner;
+
+public class P2018_006 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+
+        while (end_index != N) {
+            if (sum == N) {
+                count++;
+                end_index++;
+                sum = sum + end_index;
+            } else if (sum > N) {
+                sum = sum - start_index;
+                start_index++;
+            } else {
+                end_index ++;
+                sum = sum + end_index;
+            }
+        }
+        System.out.println(count);
+    }
+}
